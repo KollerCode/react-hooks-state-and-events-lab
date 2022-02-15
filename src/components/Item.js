6e43d1ee5772c50d5a2acd4c9874cdeb2d0b1869
Natasha Koller <kollernatasha@gmail.com>
2022-02-15 09:49:37 -0500

    swtiched add to carts in react

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleCart() {
+    setIsInCart((isInCart) => !isInCart)
+  }
+
+  const cart = isInCart ? "in-cart" : ""
+    
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart ? "remove" : "add"}
+        onClick={handleCart}>
+        {isInCart ? "Remove From" : "Add to"} Cart
+        </button>
     </li>
   );
 }
